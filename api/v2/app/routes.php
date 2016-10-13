48ddd7ae53a74699670d10e6aec17fd9afcbdedc
boujnane monssef <m-heroine@hotmail.com>
2016-10-13 18:35:45 +0100

    adding achats

--- api/v2/app/routes.php
+++ api/v2/app/routes.php
@@ -1,10 +1,16 @@
 <?php
+
 // Routes
 $app->get('/products', 'App\Controller\ProductsController:getList');
-$app->get('/product/{id}', 'App\Controller\ProductsController:getProduct');
+$app->post('/product', 'App\Controller\ProductsController:save');
 $app->get('/product/upm/{id}/date[/{year}[/{month}[/{day}]]]', 'App\Controller\ProductsController:getUpm');
-$app->get('/products/grouped', 'App\Controller\ProductsController:getGrouped');
+$app->get('/product/grouped', 'App\Controller\ProductsController:getGrouped');
+$app->get('/product/{id}', 'App\Controller\ProductsController:getProduct');
+$app->get('/productTypes', 'App\Controller\ProductsController:productTypes');
 
+$app->get('/achats', 'App\Controller\ProductsController:todayProducts');
+$app->post('/achats', 'App\Controller\ProductsController:filterProducts');
+$app->put('/achat', 'App\Controller\ProductsController:updateAchat');
 
 $app->post('/login', 'App\Controller\AuthController:doLogin');
 $app->get('/logout', 'App\Controller\AuthController:doLogout');
@@ -15,10 +21,12 @@ $app->get('/dashboard', 'App\Controller\HomeController:dashboard');
 $app->get('/stock/date[/{year}[/{month}[/{day}]]]', 'App\Controller\StockController:getList');
 $app->delete('/stock/{id}', 'App\Controller\StockController:delete');
 
+
 $app->get('/fournisseurs', 'App\Controller\FournisseursController:getList');
 $app->post('/fournisseurs', 'App\Controller\FournisseursController:save');
 $app->put('/fournisseurs', 'App\Controller\FournisseursController:update');
 $app->delete('/fournisseurs/{id}', 'App\Controller\FournisseursController:delete');
+
 
 $app->get('/clients', 'App\Controller\ClientsController:getList');
 $app->post('/clients', 'App\Controller\ClientsController:save');
--- app/controllers/achats.js
+++ app/controllers/achats.js
@@ -1,22 +1,122 @@
-app.controller('achatsCtrl', function ($scope, $state, $rootScope, $http, Data) {
+app.directive('bindOnce', function() {
+    return {
+        scope: true,
+        link: function( $scope, $element ) {
+            setTimeout(function() {
+                $scope.$destroy();
+            }, 0);
+        }
+    }
+});
+app.controller('achatsCtrl', function ($scope, $state, $rootScope, $http, Data, ngDialog) {
+  $scope.todayAchats = []
+  $scope.stock = {qty: 1, price: 0}
     $scope.go = function(state){
       $state.go(state);
     }
     $scope.loadData = function(){
-      Data.get('product/grouped').then(function(resp){
+      Data.get('productTypes').then(function(resp){
         if('status' in resp && resp.status == '200'){
-          $scope.products = resp.products;
+          $scope.productTypes = resp.productTypes;
           $scope.isLoaded = true;
-          $scope.cantLoad = true;
+          $scope.cantLoad = false;
         }else{
-          $scope.isLoaded = true;
+          $scope.isLoaded = false;
           $scope.cantLoad = true;
         }
 
       }, function(err){
-        $scope.isLoaded = true;
+        $scope.isLoaded = false;
+        $scope.cantLoad = true;
+      })
+
+      Data.get('fournisseurs').then(function(resp){
+        if('status' in resp && resp.status == '200'){
+          $scope.fournisseurs = resp.fournisseurs;
+          $scope.isLoaded = true;
+          $scope.cantLoad = false;
+        }else{
+          $scope.isLoaded = false;
+          $scope.cantLoad = true;
+        }
+
+      }, function(err){
+        $scope.isLoaded = false;
+        $scope.cantLoad = true;
+      })
+
+      Data.post('achats').then(function(resp){
+        if('status' in resp && resp.status == '200'){
+          $scope.todayAchats = resp.products;
+          $scope.isLoaded = true;
+          $scope.cantLoad = false;
+        }else{
+          $scope.isLoaded = false;
+          $scope.cantLoad = true;
+        }
+
+      }, function(err){
+        $scope.isLoaded = false;
         $scope.cantLoad = true;
       })
     }
+
     $scope.loadData();
+
+    $scope.saveAchat = function(){
+      Data.post('product', $scope.stock).then(function(resp){
+        if('status' in resp && resp.status == '200'){
+          $scope.todayAchats.push(resp.product);
+        }else{
+          
+        }
+
+      }, function(err){
+        
+      })
+    }
+    $scope.ShowEditAchat =  function(){
+      if(!$scope.selected){
+        alert('Selection un achat a moddifier!');
+        return;
+      }
+      ngDialog.open({
+        template: '/partials/modals/achat.edit.html',
+        className: 'ngdialog-theme-plain',
+        scope: $scope,
+        controller: function($scope, Data) {
+          $scope.editing = $scope.selected;
+          $scope.save = function(){
+              Data.put('achat', $scope.editing).then(function(resp){
+                if('status' in resp && resp.status == '200'){
+                  console.log('resp',resp.achat)
+                }else{
+                  
+                }
+
+                }, function(err){
+                  
+                })
+              }
+        }
+      });
+    }
+
+    $scope.select = function(prod){
+      $scope.selected = prod;
+    }
+
+    $scope.filterAchats = function(){
+      console.log('filterDates',$scope.filterDates);
+      Data.post('achats', $scope.filterDates).then(function(resp){
+        if('status' in resp && resp.status == '200'){
+          $scope.todayAchats = resp.products;
+        }else{
+          
+        }
+
+      }, function(err){
+        
+      })
+    }
 });
